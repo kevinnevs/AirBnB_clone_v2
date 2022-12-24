8bfcbb55251315be4dd5c4b7be81751656f23be3
Kevin Njuguna <kevinnjuguna2@gmail.com>
2022-12-24 22:28:29 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,45 +1,36 @@
 #!/usr/bin/python3
 """
-Fabricp script that distributes archive to web servers
-using function do_deploy
+Fabric script method:
+    do_deploy: deploys archive to webservers
+Usage:
+    fab -f 2-do_deploy_web_static.py
+    do_deploy:archive_path=versions/web_static_20170315003959.tgz
+    -i my_ssh_private_key -u ubuntu
 """
-from fabric.api import env, put, run, local
+from fabric.api import env, put, run
 import os.path
 env.hosts = ['18.209.225.104', '100.26.209.134']
 
+
 def do_deploy(archive_path):
     """
-    Deploy archive to the web server
+    Deploy archive to web server
     """
     if os.path.isfile(archive_path) is False:
         return False
     try:
-        file_name = archive_path.split("/")[-1]
-        release_dir = "/data/web_static/releases/{}".format(file_name.split(".")[0])
-
-        # upload archive to server
-        put(archive_path, "/tmp/{}".format(file_name))
-
-        # create release directory
-        run("mkdir -p {}".format(release_dir))
-
-        # uncompress archive
-        run("tar -xzf /tmp/{} -C {}".format(file_name, release_dir))
-
-        # remove archive
-        run("rm /tmp/{}".format(file_name))
-
-        # move files to release directory
-        run("mv {}web_static/* {}".format(release_dir, release_dir))
-
-        # remove web_static directory
-        run("rm -rf {}web_static".format(release_dir))
-
-        # delete current symlink
-        run("rm -rf /data/web_static/current")
-
-        # create new symlink to release directory
-        run("ln -s {} /data/web_static/current".format(release_dir))
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
         return True
     except:
         return False
