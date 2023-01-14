c325744696b5e0d4adc55c74bbf2934db8ce0c47
kevinnevs <kevinnjuguna2@gmail.com>
2023-01-14 13:23:15 +0000

    fixing pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    function for hello hbhb
+    """
     return "Hello, HBNB!"
 
 if __name__ == "__main__":
