afc66c1ecfb2563ee75aeb07237172152c67f37b
Kevin Njuguna <kevinnjuguna2@gmail.com>
2023-01-16 12:35:18 +0300

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,9 @@
 -- MySQL Script that prepares the server for this project
+-- GRANT USAGE ON *.* TO 'hbnb_dev'@'locahost'
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL on hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
